80182d204225bdf138df7ffbf3a7df726628dcd5
lexa-bobexa <alexa.kopf@gmail.com>
2026-01-27 15:37:22 -0600

    Add multiple news articles to capacity_news.json and update Accept-Encoding in utils.py

--- scrapers/news/providers/utils.py
+++ scrapers/news/providers/utils.py
@@ -46,7 +46,7 @@ def build_headers(url):
         "User-Agent": random.choice(USER_AGENTS),
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "gzip, deflate",
         "DNT": "1",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
